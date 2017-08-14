cea112120c12bf1afae7db0510c95b28234a7361
Sneh22 <30681411+Sneh22@users.noreply.github.com>
2017-08-14 23:04:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //changing the html content
 var element=document.getElementById('main-text');
 element.innerHTML='This is the new and changed text';
+//move the image with click
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
