eaf206b9181d6900c66e8f1dac3488f843fcf912
Sneh22 <30681411+Sneh22@users.noreply.github.com>
2017-08-14 22:58:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //changing the html content
-var element=document.getlElementById('main-text');
+var element=document.getElementById('main-text');
 element.innerHTML='This is the new and changed text';
